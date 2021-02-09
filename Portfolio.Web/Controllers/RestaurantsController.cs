81dbec745a22b24e58411fedf217f608a160f0f5
Michal Izdebski <theetescoo@gmail.com>
2021-02-09 17:52:51 +0100

    Added a restaurants controller as well as a view with it

---
+++ Portfolio.Web/Controllers/RestaurantsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Portfolio.Data.Services;
+
+namespace Portfolio.Web.Controllers
+{
+    public class RestaurantsController : Controller
+    {
+        private readonly IRestaurantData _db;
+
+        public RestaurantsController(IRestaurantData db)
+        {
+            _db = db;
+        }
+        // GET: Restaurants
+        public ActionResult Index()
+        {
+            var model = _db.GetAll();
+            return View(model);
+        }
+    }
+}
